10a28634678deff0c7bae378265a5d309de21810
rajesjad <rajesjad@DIN55001778.corp.capgemini.com>
2019-06-26 16:19:40 +0530

    dcsd

---
+++ collection/src/collection/MainClass.java
@@ -0,0 +1,18 @@
+package collection;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ int num=100;
+ Integer iob=num;
+ System.out.println(iob);
+ System.out.println(num);
+ 
+ Integer iob1=new Integer(200);
+ int num1=iob1;
+ System.out.println(iob1);
+ System.out.println(num1);
+	}
+
+}
